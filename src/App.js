bf2fcf181205f95bf5bf0ff3f21902b66ad26ea8
DeBasser <debasser1911@gmail.com>
2019-03-17 22:18:57 +0200

    axios add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    respData: [],
+  }
+  componentDidMount(){
+    axios.get('http://localhost:5555').then(response => {
+      this.setState({respData:response.data})
+      console.log(response.data)
+      console.log(this.state.respData)
+    });
+
+  }
+  
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>
+{this.state.respData}
+</div>
+
     );
   }
 }
