45a50ea9ebb631e1b090cbbd055544a3fa91240d
DeBasser <debasser1911@gmail.com>
2019-03-17 23:00:51 +0200

    openWeather add

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
   state = {
-    respData: [],
-  }
-  componentDidMount(){
-    axios.get('http://localhost:5555').then(response => {
-      this.setState({respData:response.data})
-      console.log(response.data)
-      console.log(this.state.respData)
-    });
+    weatherData: {},
+    temp: {},
+    wind: {},
+    coord: {},
 
+
+
+  };
+  componentDidMount() {
+    axios
+      .get(
+        "http://api.openweathermap.org/data/2.5/weather?q=Kiev&units=metric&appid=b0ce1fbd2b74865c3ef64d68aad23248"
+      )
+      .then(response => {
+        this.setState({ weatherData: response.data, temp: response.data.main, wind: response.data.wind, coord: response.data.coord });
+        console.log(response.data);
+        console.log(this.state.temp);
+        console.log(this.state.wind);
+      });
   }
-  
 
   render() {
     return (
       <div>
-{this.state.respData}
-</div>
-
+        {JSON.stringify(this.state.weatherData)}
+        <ul>
+          <li>Долгота: {this.state.coord.lon}</li>
+          <li>Широта: {this.state.coord.lat}</li>
+          <li>Температура: {this.state.temp.temp}</li>
+          <li>Ветер: {this.state.wind.speed}</li>
+        </ul>
+      </div>
     );
   }
 }
