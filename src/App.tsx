49b2046eac47f6a42066a30fb6789c680817cee5
debasser1911 <debasser1911@gmail.com>
2022-06-01 22:38:41 +0300

    feat: some work done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Form, Switch } from 'antd';
+import { useState } from 'react';
+import { AttributeI, mockedAttributes } from './mockData';
+import { FieldData } from 'rc-field-form/es/interface';
 
 function App() {
+  const onFinish = (allValues: any) => {
+    console.log(allValues);
+  };
+
+  const [availableAttributes, setAvailableAttributes] =
+    useState<AttributeI[]>(mockedAttributes);
+  const [enabledAttributes, setEnabledAttributes] = useState<string[]>([]);
+
+  const onChange = (changed: any): void => {
+    if (enabledAttributes.includes(changed[0].name[0])) {
+      const newArray = enabledAttributes.splice(
+        enabledAttributes.findIndex(
+          (attribute) => attribute === changed[0].name
+        ),
+        1
+      );
+      setEnabledAttributes(newArray);
+    } else {
+      setEnabledAttributes([
+        ...enabledAttributes,
+        changed[0].name[0] as string,
+      ]);
+    }
+    console.log(changed);
+    console.log(enabledAttributes);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Form onFinish={onFinish} onFieldsChange={onChange}>
+        {availableAttributes.map((attribute) => (
+          <Form.Item name={attribute.title} label={attribute.displayName}>
+            <Switch />
+          </Form.Item>
+        ))}
+        <Form.Item>
+          <Button htmlType='submit'>submit</Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
---
+++ src/mockData.ts
@@ -0,0 +1,23 @@
+export interface AttributeI {
+  title: string;
+  displayName: string;
+  id: number;
+}
+
+export const mockedAttributes = [
+  {
+    title: 'super-atttibute',
+    displayName: 'Super attribute',
+    id: 12345,
+  },
+  {
+    title: 'duper-atttibute',
+    displayName: 'Duper attribute',
+    id: 54321,
+  },
+  {
+    title: 'ololo-atttibute',
+    displayName: 'Ololo attribute',
+    id: 11111,
+  },
+];
